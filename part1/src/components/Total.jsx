16e6e28f5b35f97fc67eb6e54ebed0d9fd29c0f7
Amir9eng <amirmukhtarme@gmail.com>
2020-11-16 18:24:02 +0100

    feat: total

--- part1/src/components/Total.jsx
+++ part1/src/components/Total.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Total({ parts }) {
   return (
     <div>
-      <p>Number of exercises { parts.reduce((acc, parts) => acc + parts.exercises, 0 )}</p>
+      <p>Number of exercises { parts.reduce(( accumulator, parts ) => accumulator + parts.exercises, 0 )}</p>
     </div>
   )
 }
