520c37151415fb7a797e19d24ed9907a36786316
Amir9eng <amirmukhtarme@gmail.com>
2020-11-20 18:16:17 +0100

    feat: javascript-trials

---
+++ javascript-trials/index.js
@@ -0,0 +1,71 @@
+const x = 1
+let y = 5
+
+console.log(x, y)   
+y += 10
+console.log(x, y)   
+y = 'sometext'
+console.log(x, y)   
+x = 4     
+
+// Objects
+
+const object1 = {
+  name: 'Arto Hellas',
+  age: 35,
+  education: 'PhD',
+}
+
+const object2 = {
+  name: 'Full Stack web application development',
+  level: 'intermediate studies',
+  size: 5,
+}
+
+const object3 = {
+  name: {
+    first: 'Dan',
+    last: 'Abramov',
+  },
+  grades: [2, 3, 5, 3],
+  department: 'Stanford University',
+}
+
+// functions
+
+const sum = (p1, p2) => {
+  console.log(p1)
+  console.log(p2)
+  return p1 + p2
+}
+
+// Object methods and "this"
+
+const arto = {
+  name: 'Arto Hellas',
+  age: 35,
+  education: 'PhD',
+  greet: function() {
+    console.log('hello, my name is ' + this.name)
+  },
+}
+
+arto.greet()
+
+
+const arto = {
+  name: 'Arto Hellas',
+  age: 35,
+  education: 'PhD',
+  greet: function() {
+    console.log('hello, my name is ' + this.name)
+  },
+}
+
+arto.growOlder = function() {
+  this.age += 1
+}
+
+console.log(arto.age)   // 35 is printed
+arto.growOlder()
+console.log(arto.age)
