714276946231bdcc6ccf2a9c50b5093915f70d9a
Amir9eng <amirmukhtarme@gmail.com>
2020-11-21 19:33:02 +0100

    javascript-trials

--- javascript-trials/index.js
+++ javascript-trials/index.js
@@ -94,3 +94,19 @@ const App = () => {
     </div>
   )
 }
+
+const Hello = (props) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
+
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
