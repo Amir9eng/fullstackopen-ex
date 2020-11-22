421e0e3d384f33dace60712cb94bb5777f25dfbe
Amir9eng <amirmukhtarme@gmail.com>
2020-11-22 14:06:49 +0100

    feat: react-trials

--- javascript-trials/index.js
+++ javascript-trials/index.js
@@ -110,3 +110,19 @@ const Hello = (props) => {
     </div>
   )
 }
+
+// Destructuring
+
+const Hello = (props) => {
+  const name = props.name
+  const age = props.age
+
+  const bornYear = () => new Date().getFullYear() - age
+
+  return (
+    <div>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
