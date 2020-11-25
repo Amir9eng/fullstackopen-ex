05e5ed92698b74ea472419dfafda1809c054e15e
Amir9eng <amirmukhtarme@gmail.com>
2020-11-25 21:41:46 +0100

    feat: javascript-trials

--- javascript-trials/index.js
+++ javascript-trials/index.js
@@ -126,3 +126,23 @@ const Hello = (props) => {
     </div>
   )
 }
+
+const App = (props) => {
+  const { counter } = props
+  return (
+    <div>{counter}</div>
+  )
+}
+
+let counter = 1
+
+const refresh = () => {
+  ReactDOM.render(<App counter={counter} />, 
+  document.getElementById('root'))
+}
+
+refresh()
+counter += 1
+refresh()
+counter += 1
+refresh()
