de01381f4b17c1deefd1e038ae86b4cd59a905bc
Amir9eng <amirmukhtarme@gmail.com>
2020-11-28 23:03:43 +0100

    feat: javascript-trials

--- javascript-trials/index.js
+++ javascript-trials/index.js
@@ -170,3 +170,29 @@ const App = () => {
     </div>
   )
 }
+
+const [ counter, setCounter ] = useState(0)
+
+const increaseByOne = () => setCounter(counter + 1)
+const decreaseByOne = () => setCounter(counter - 1)
+const setToZero = () => setCounter(0)
+
+return (
+  <div>
+    <Display counter={counter}/>
+    <Button
+      handleClick={increaseByOne}
+      text='plus'
+    />
+    <Button
+      handleClick={setToZero}
+      text='zero'
+    />     
+    <Button
+      handleClick={decreaseByOne}
+      text='minus'
+    />           
+  </div>
+)
+}
+
