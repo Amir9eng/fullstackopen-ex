db5576ce8fa959c6b1ae6ab7efc413d0511fc564
Amir9eng <amirmukhtarme@gmail.com>
2020-12-08 23:23:22 +0100

    feat: js-trials

--- part1/src/components/Unicafe.jsx
+++ part1/src/components/Unicafe.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
  const Unicafe = () => {
-  const [good, setGood] = useState(1)
+  const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   return (
