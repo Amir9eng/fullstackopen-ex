be3e8aab904d0c9a57389b2252176711d82fbb33
Amir9eng <amirmukhtarme@gmail.com>
2021-01-24 18:07:01 +0100

    WIP

--- javascript-trials/index.js
+++ javascript-trials/index.js
@@ -1,76 +1,75 @@
-const x = 1
-let y = 5
+const x = 1;
+let y = 5;
 
-console.log(x, y)   
-y += 10
-console.log(x, y)   
-y = 'sometext'
-console.log(x, y)   
-x = 4     
+console.log(x, y);
+y += 10;
+console.log(x, y);
+y = "sometext";
+console.log(x, x);
+x = 4;
 
 // Objects
 
 const object1 = {
-  name: 'Arto Hellas',
+  name: "Arto Hellas",
   age: 35,
-  education: 'PhD',
-}
+  education: "PhD",
+};
 
 const object2 = {
-  name: 'Full Stack web application development',
-  level: 'intermediate studies',
+  name: "Full Stack web application development",
+  level: "intermediate studies",
   size: 5,
-}
+};
 
 const object3 = {
   name: {
-    first: 'Dan',
-    last: 'Abramov',
+    first: "Dan",
+    last: "Abramov",
   },
   grades: [2, 3, 5, 3],
-  department: 'Stanford University',
-}
+  department: "Stanford University",
+};
 
 // functions
 
 const sum = (p1, p2) => {
-  console.log(p1)
-  console.log(p2)
-  return p1 + p2
-}
+  console.log(p1);
+  console.log(p2);
+  return p1 + p2;
+};
 
 // Object methods and "this"
 
 const arto = {
-  name: 'Arto Hell',
+  name: "Arto Hell",
   age: 35,
-  education: 'PhD',
-  greet: function() {
-    console.log('hello, my name is ' + this.name)
+  education: "PhD",
+  greet: function () {
+    console.log("hello, my name is " + this.name);
   },
-}
+};
 
-arto.greet()
-
+arto.greet();
 
 const arto = {
-  name: 'Arto Hellas',
+  name: "Arto Hellas",
   age: 35,
-  education: 'PhD',
-  greet: function() {
-    console.log('hello, my name is ' + this.name)
+  education: "PhD",
+  greet: function () {
+    console.log("hello, my name is " + this.name);
   },
-}
+};
 
-arto.growOlder = function() {
-  this.age += 1
-}
+arto.growOlder = function () {
+  this.age += 1;
+};
 
-console.log(arto.age)   // 35 is printed
-arto.growOlder()
-console.log(arto.age)
+console.log(arto.age); // 35 is printed
+arto.growOlder();
+console.log(arto.age);
 
-// props 
+// props
 
 const Hello = (props) => {
   return (
@@ -79,12 +78,12 @@ const Hello = (props) => {
         Hello {props.name}, you are {props.age} years old
       </p>
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const name = "Peter";
+  const age = 10;
 
   return (
     <div>
@@ -92,14 +91,14 @@ const App = () => {
       <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
     </div>
-  )
-}
+  );
+};
 
 const Hello = (props) => {
   const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - props.age
-  }
+    const yearNow = new Date().getFullYear();
+    return yearNow - props.age;
+  };
 
   return (
     <div>
@@ -108,98 +107,79 @@ const Hello = (props) => {
       </p>
       <p>So you were probably born in {bornYear()}</p>
     </div>
-  )
-}
+  );
+};
 
 // Destructuring
 
 const Hello = (props) => {
-  const name = props.name
-  const age = props.age
+  const name = props.name;
+  const age = props.age;
 
-  const bornYear = () => new Date().getFullYear() - age
+  const bornYear = () => new Date().getFullYear() - age;
 
   return (
     <div>
-      <p>Hello {name}, you are {age} years old</p>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
       <p>So you were probably born in {bornYear()}</p>
     </div>
-  )
-}
+  );
+};
 
 const App = (props) => {
-  const { counter } = props
-  return (
-    <div>{counter}</div>
-  )
-}
+  const { counter } = props;
+  return <div>{counter}</div>;
+};
 
-let counter = 1
+let counter = 1;
 
 const refresh = () => {
-  ReactDOM.render(<App counter={counter} />, 
-  document.getElementById('root'))
-}
+  ReactDOM.render(<App counter={counter} />, document.getElementById("root"));
+};
 
-refresh()
-counter += 1
-refresh()
-counter += 1
-refresh()
+refresh();
+counter += 1;
+refresh();
+counter += 1;
+refresh();
 
 setInterval(() => {
-  refresh()
-  counter += 1
-}, 1000)
+  refresh();
+  counter += 1;
+}, 1000);
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [counter, setCounter] = useState(0);
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const setToZero = () => setCounter(0)
+  const increaseByOne = () => setCounter(counter + 1);
+  const setToZero = () => setCounter(0);
 
   return (
     <div>
-      <Display counter={counter}/>
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-      <button onClick={setToZero}> 
-        zero
-      </button>
+      <Display counter={counter} />
+      <button onClick={increaseByOne}>plus</button>
+      <button onClick={setToZero}>zero</button>
     </div>
-  )
-}
+  );
+};
 
-const [ counter, setCounter ] = useState(0)
+const [counter, setCounter] = useState(0);
 
-const increaseByOne = () => setCounter(counter + 1)
-const decreaseByOne = () => setCounter(counter - 1)
-const setToZero = () => setCounter(0)
+const increaseByOne = () => setCounter(counter + 1);
+const decreaseByOne = () => setCounter(counter - 1);
+const setToZero = () => setCounter(0);
 
 return (
   <div>
-    <Display counter={counter}/>
-    <Button
-      handleClick={increaseByOne}
-      text='plus'
-    />
-    <Button
-      handleClick={setToZero}
-      text='zero'
-    />     
-    <Button
-      handleClick={decreaseByOne}
-      text='minus'
-    />           
+    <Display counter={counter} />
+    <Button handleClick={increaseByOne} text="plus" />
+    <Button handleClick={setToZero} text="zero" />
+    <Button handleClick={decreaseByOne} text="minus" />
   </div>
-)
-const Display = ({ counter }) => <div>{counter}</div>
+);
+const Display = ({ counter }) => <div>{counter}</div>;
 const Button = (props) => {
-  return (
-    <button onClick={props.handleClick}>
-      {props.text}
-    </button>
-  )
-}
-
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
