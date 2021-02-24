12abfb00e29d614de8e34c1617908cbf6d44edb2
Amir9eng <amirmukhtarme@gmail.com>
2021-02-24 12:54:50 +0100

    Refactor and update

--- part2/classwork/src/App.js
+++ part2/classwork/src/App.js
@@ -1,77 +1,69 @@
-import React, { useEffect, useState } from "react";
-import Axios from "axios";
-import noteService from "./services/notes";
-import Note from "./components/Note";
+import React, { useEffect, useState } from "react"
+import Axios from "axios"
+import noteService from "./services/notes"
+import Note from "./components/Note"
 
 const App = () => {
-  const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState("add a new note");
-  const [showAll, setShowAll] = useState(true);
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState("add a new note")
+  const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
-    noteService.getAll().then((initialNotes) => setNotes(initialNotes));
-  }, []);
+    noteService.getAll().then((initialNotes) => setNotes(initialNotes))
+  }, [])
 
   const toggleImportanceOf = (id) => {
-    const note = notes.find((n) => n.id === id);
-    const changedNote = { ...note, important: !note.important };
+    const note = notes.find((n) => n.id === id)
+    const changedNote = { ...note, important: !note.important }
 
     noteService
       .update(id, changedNote)
       .then((returnedNotes) => {
-        setNotes(notes.map((note) => (note.id !== id ? note : returnedNotes)));
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNotes)))
       })
-      .catch((error) => alert(`the note '${note.content}' was deleted`));
-    setNotes(notes.filter((n) => n.id !== id));
-  };
+      .catch((error) => alert(`the note '${note.content}' was deleted`))
+    setNotes(notes.filter((n) => n.id !== id))
+  }
 
   const addNote = (event) => {
-    event.preventDefault();
-    console.log("button clicked", event.target);
+    event.preventDefault()
+    console.log("button clicked", event.target)
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
       id: notes.length + 1,
-    };
-
-    if (!newNote.trim()) {
-      alert("ogbeni , write something there jare");
-      return;
     }
 
-    // setNotes(notes.concat(noteObject));
-    // setNewNote(" ");
-
-    // Axios.get("http://localhost:3003/notes").then((response) => {
-    //   const notes = response.data;
-    //   console.log(notes);
-    // });
+    if (!newNote.trim()) {
+      alert("ogbeni , write something there jare")
+      return
+    }
 
     noteService.create(noteObject).then((returnedNotes) => {
-      setNotes(notes.concat(returnedNotes));
-      setNewNote("");
-    });
+      setNotes(notes.concat(returnedNotes))
+      setNewNote("")
+    })
 
     Axios.post(
       "https://arcane-tor-55493.herokuapp.com/api/notes",
       noteObject
     ).then((response) => {
-      console.log(response, "success!");
-      setNotes(notes.concat(response.data));
-      setNewNote("");
-    });
-  };
+      console.log(response, "success!")
+      setNotes(notes.concat(response.data))
+      setNewNote("")
+    })
+  }
 
   const handleNewNote = (event) => {
-    console.log(event.target.value);
-    setNewNote(event.target.value);
-  };
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
 
-  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
-  console.log(notes);
-  console.log(notesToShow);
+  console.log(notes)
+  console.log(notesToShow)
 
   return (
     <div>
@@ -95,7 +87,7 @@ const App = () => {
         <button type="submit">save</button>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
